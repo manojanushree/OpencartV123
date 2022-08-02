0125103b68d8c54917145c23ced001a80d6e1e8c
manojanushree <manoj2486@gmail.com>
2022-08-01 21:04:15 -0400

    This is my 2nd commit on 01 Aug

--- src/test/java/pageObjects/MyAccountPage.java
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -5,21 +5,63 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class MyAccountPage {
-WebDriver driver;
+public class MyAccountPage 
+{
+	WebDriver driver;
 	
 	public MyAccountPage(WebDriver driver)
 	{
 		this.driver=driver;
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver,this);
 	}
+
+	@FindBy(xpath="//input[@id='input-email']")
+	WebElement txtEmailAddress;
 	
-	@FindBy(xpath="//div[@class='list-group']//a[text()='Logout']")
-	WebElement lnkLogout;
+	@FindBy(xpath="//input[@id='input-password']")
+	WebElement txtPassword;
 	
-	public void clickLogout()
+	@FindBy(xpath="//input[@value='Login']")
+	WebElement btnLogin;
+	
+	@FindBy(xpath="//a[text()='Order History']")
+	WebElement orderhistory;
+	
+	 @FindBy(xpath="//div[@class='list-group']//a[text()='Logout']")
+		WebElement lnkLogout;
+	 
+	 @FindBy(xpath="//a[text()='Site Map']")
+	 WebElement sitemap;
+	   
+	   public void clickLogout()
+	   {
+		   lnkLogout.click();
+		 
+	   }
+	
+	public void setEmail(String email)
 	{
-		lnkLogout.click();
+		txtEmailAddress.sendKeys(email);
 	}
-		
+	
+	public void setPassword(String pwd)
+	{
+		txtPassword.sendKeys(pwd);
+	}
+	
+	public void clickLogin()
+	{
+		btnLogin.click();
+	}
+	
+	public void order_history()
+	{
+		orderhistory.click();
+	}
+	
+	public void site_map()
+	{
+		sitemap.click();
+	}
+	
 }
--- src/test/java/testCases/TC_004_Search.java
+++ src/test/java/testCases/TC_004_Search.java
@@ -1,7 +1,64 @@
 package testCases;
 
-public class TC_004_Search {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-	//adding new dummy test case for github
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.SearchPage;
+import testBase.BaseClass;
 
-}
+@Test(groups={"sanity"})
+public class TC_004_Search extends BaseClass
+{
+	 
+	public void  test_search() throws InterruptedException
+		{
+		logger.info(" Starting TC_004_Search ");
+		
+		try
+		{
+			
+ 		   driver.get(rb.getString("appURL"));
+		   HomePage hp=new HomePage(driver);
+		   logger.info("Home Page Displayed ");
+		
+		   driver.manage().window().maximize();
+		
+		    SearchPage sp=new SearchPage(driver);
+		
+		 //  sp.searchtxt("iPhoness");
+		   sp.searchtxt("iMac");
+		    Thread.sleep(2000);
+		     logger.info("Enter any Product"); 
+		
+		    sp.searchbutton();
+		    Thread.sleep(1000);
+		    
+		   
+		    boolean tarpage=sp.isPageExists();
+		    
+		    if(tarpage)
+		    {
+		    	logger.info("Search Failed ");
+				Assert.assertTrue(true);
+		    }
+		    
+		    else
+			{
+				logger.error("Search Success ");
+				captureScreen(driver, "test_search"); //Capturing screenshot
+				Assert.assertTrue(true);
+			}
+		}	
+		catch(Exception e)
+		{
+			logger.fatal("Search Failed ");
+			Assert.fail();
+		}
+		
+		logger.info(" Finished TC_004Search ");
+		
+	}
+	     }
